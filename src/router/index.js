ebe0e5d351d829100ced098b0e5692b6aa5548bc
ysj18614959926 <18614959926@163.com>
2019-06-21 15:29:48 +0800

    添加了elementUI，网站所用到的静态图片

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+const Home = () => import('./views/Home.vue')
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+    path: '/',
+    component: Home
+  }]
 })
