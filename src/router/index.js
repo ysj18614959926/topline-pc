9339f2ba1cfff87d16e3c0635a1a9d6e53a0cb8e
ysj18614959926 <18614959926@163.com>
2019-06-24 20:31:02 +0800

    路由拦截，如果不登录只能去login页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
-
-export default new Router({
+const getUser = window.localStorage.getItem('userLogin')
+const router = new Router({
   routes: [{
     path: '/',
     component: () => import('@/views/layout'),
@@ -24,3 +24,19 @@ export default new Router({
     component: () => import('@/views/login')
   }]
 })
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    if (getUser) {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (getUser) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
+export default router
