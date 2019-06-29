befe1337827a571765fffa6edf8c706664711e9a
ysj18614959926 <18614959926@163.com>
2019-06-29 20:20:30 +0800

    quill富文本编辑器

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,34 +1,45 @@
 <template>
     <div>
-       <el-form ref="publish" :model="publish" label-width="80px">
-        <el-form-item label="标题">
-            <el-input v-model="publish.title"></el-input>
-        </el-form-item>
-        <el-form-item label="内容">
-            <el-input v-model="publish.content"></el-input>
-        </el-form-item>
-        <el-form-item label="封面">
-            <el-radio-group  v-model="publish.cover">
-                <el-radio label="线上品牌商赞助"></el-radio>
-                <el-radio label="线下场地免费"></el-radio>
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label='频道'>
-            <artical-channel v-model='publish.channel_id'></artical-channel>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="success" @click="handelAddArtical(true)">发布</el-button>
-            <el-button type='warning' @click='handelAddArtical(false)'>草稿</el-button>
-        </el-form-item>
-        </el-form>
+      <el-col :span='20'>
+        <el-form ref="publish" :model="publish" label-width="80px">
+         <el-form-item label="标题">
+             <el-input v-model="publish.title"></el-input>
+         </el-form-item>
+         <el-form-item label="内容">
+             <quill-editor v-model="publish.content"
+               ref="myQuillEditor"
+               :options="editorOption"
+               >
+            </quill-editor>
+         </el-form-item>
+         <el-form-item label="封面">
+             <el-radio-group  v-model="publish.cover">
+                 <el-radio label="线上品牌商赞助"></el-radio>
+                 <el-radio label="线下场地免费"></el-radio>
+             </el-radio-group>
+         </el-form-item>
+         <el-form-item label='频道'>
+             <artical-channel v-model='publish.channel_id'></artical-channel>
+         </el-form-item>
+         <el-form-item>
+             <el-button type="success" @click="handelAddArtical(false)">发布</el-button>
+             <el-button type='warning' @click='handelAddArtical(true)'>草稿</el-button>
+         </el-form-item>
+         </el-form>
+      </el-col>
     </div>
 </template>
 <script>
 import ArticalChannel from '@/components/ArticalChannel'
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
   name: 'publish',
   components: {
-    ArticalChannel
+    ArticalChannel,
+    quillEditor
   },
   data () {
     return {
@@ -41,7 +52,8 @@ export default {
         },
         channel_id: null,
         imgType: 'none'
-      }
+      },
+      editorOption: {} // 自定义选项
     }
   },
   methods: {
@@ -67,3 +79,5 @@ export default {
   }
 }
 </script>
+<style>
+</style>
